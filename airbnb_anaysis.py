5e5d0ed1bdf6b0cec1e9675f6a29692dfaaf5a6b
Bibek Poudel <131543477+bIBEKpoudel86@users.noreply.github.com>
2025-03-04 18:19:08 +0545

    little

--- airbnb_anaysis.py
+++ airbnb_anaysis.py
@@ -3,4 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('AB_NYC_2019/AB_NYC_2019.csv')
+df = pd.read_csv('AB_NYC_2019/AB_NYC_2019.csv') #load dataset
+
+#display basic info
+print("Dataset info:\n")
+print(df.info())
+print("\nFirst five rows:\n")
+print(df.head())
